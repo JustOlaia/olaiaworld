d7e4d91d28534213b9363a5dd42b6077500e5fef
JustOlaia <104322336+JustOlaia@users.noreply.github.com>
2022-06-22 15:36:13 -0500

    Delete tools directory

--- tools/msys2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title MSYS2
-start https://github.com/msys2/msys2-installer/releases
-goto exit
--- tools/sm64pcbuilder2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-title sm64pcbuilder2
-start https://sm64pc.info/sm64pcbuilder2/
-goto exit
